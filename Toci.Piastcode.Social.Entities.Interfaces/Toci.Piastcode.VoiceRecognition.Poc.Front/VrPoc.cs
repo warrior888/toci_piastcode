acab540121ca469251c54e8e7031bdd309efda1b
Bartek Zapart <bzapart@gmail.com>
2017-06-09 21:54:57 +0200

    speech recognition initial poc tests

---
+++ Toci.Piastcode.Social.Entities.Interfaces/Toci.Piastcode.VoiceRecognition.Poc.Front/VrPoc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Toci.Piastcode.VoiceRecognition.Poc.Audio2Text;
+
+namespace Toci.Piastcode.VoiceRecognition.Poc.Front
+{
+    public partial class VrPoc : Form
+    {
+        public VrPoc()
+        {
+            InitializeComponent();
+            Button micStart = new Button();
+            micStart.Text = "Start recording";
+            micStart.Size = new Size(80, 20);
+            micStart.Location = new Point(20, 20);
+            micStart.Click += MicStart_Click;
+
+            Controls.Add(micStart);
+        }
+
+        private void MicStart_Click(object sender, EventArgs e)
+        {
+            MicPoc poc = new MicPoc();
+
+            poc.MicPocTest();
+        }
+    }
+}
---
+++ Toci.Piastcode.Social.Entities.Interfaces/Toci.Piastcode.VoiceRecognition.Poc/Audio2Text/MicPoc.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Speech.Recognition;
+using Microsoft.Win32.SafeHandles;
+using SpeechLib.Models;
+using SpeechLib.Recognition;
+
+namespace Toci.Piastcode.VoiceRecognition.Poc.Audio2Text
+{
+    public class MicPoc
+    {
+        public void MicPocTest()
+        {
+            SpeechRecognition sp = new SpeechRecognition();
+
+            StreamReader sr = new StreamReader(@"C:\Users\bzapart\Documents\toci_piastcode\Toci.Piastcode.Social.Entities.Interfaces\Toci.Piastcode.VoiceRecognition.Poc\data\grammar.xml");
+
+            sp.LoadGrammar(sr.ReadToEnd().ToLower(), "exGrammar");
+
+            sp.SpeechRecognitionEngine.SpeechDetected += SpeechRecognitionEngine_SpeechDetected;
+            
+
+            sp.SetInputToDefaultAudioDevice();
+            sp.Recognized += HandleRecognition;
+            sp.NotRecognized += Sp_NotRecognized;
+            sp.Start();
+
+            AudioState state = sp.SpeechRecognitionEngine.AudioState;
+            //sp.Stop();
+        }
+
+        private void SpeechRecognitionEngine_SpeechDetected(object sender, System.Speech.Recognition.SpeechDetectedEventArgs e)
+        {
+            
+        }
+
+        private void Sp_NotRecognized(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        public void HandleRecognition(object sender, SpeechRecognitionEventArgs e)
+        {
+            
+        }
+    }
+}
